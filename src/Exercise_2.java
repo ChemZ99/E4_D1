43857a08c578a39ca3af472dc6a31627ab2b765b
Andrea Cavallo <astarore@gmail.com>
2023-10-02 15:05:24 +0200

    added exercise 2

---
+++ src/Exercise_2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Exercise_2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("scrivi qualcosa");
+        String text1 = input.nextLine();
+        System.out.println("hai scritto" + text1);
+        System.out.println("ora scrivi un numero");
+        int num1 = Integer.parseInt(input.nextLine());
+        System.out.println("hai scritto" + num1);
+        System.out.println("scrivi qualcos'altro");
+        String text2 = input.nextLine();
+        System.out.println("hai scritto" + text2);
+        System.out.println("risultato"+text1+num1+text2);
+        System.out.println("risultato invertito"+text2+num1+text1);
+    }
+}
